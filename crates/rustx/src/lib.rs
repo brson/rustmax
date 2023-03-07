3457386801e34ede4393be000de34e6da4c429b3
Brian Anderson <andersrb@gmail.com>
2023-03-06 23:38:03 -0700

    .

--- crates/rustx/src/lib.rs
+++ crates/rustx/src/lib.rs
@@ -12,7 +12,36 @@
 //!   - `anyhow/backtrace`
 //! - `all-crates`
 
+//! todo
+//!
+//! - update big_s and og_fmt to be no_std
+//! - update big_s to `use format as fmt`
+
+
+/* ---------- */
+
+
 #![no_std]
+
+
+/* ---------- */
+
+
+pub mod prelude {
+    //! The `rustx` prelude.
+
+    /* ---------- */
+
+    #[cfg(feature = "big_s")]
+    pub use big_s::S;
+
+    #[cfg(feature = "og_fmt")]
+    pub use og_fmt::fmt;
+}
+
+
+/* ---------- */
+
 
 pub extern crate core;
 
@@ -22,18 +51,45 @@ pub extern crate alloc;
 pub extern crate std;
 
 
-      
+/* ---------- */
+
+
 #[cfg(feature = "anyhow")]
-pub use anyhow;
+pub mod anyhow {
+    //! TODO
+    //!
+    //! See crate [`::anyhow`].
+    pub use ::anyhow::*;
+}
 
 #[cfg(feature = "big_s")]
-pub use big_s;
+pub mod big_s {
+    //! TODO
+    //!
+    //! See crate [`::big_s`].
+    pub use ::big_s::*;
+}
 
 #[cfg(feature = "blake3")]
-pub use blake3;
+pub mod blake3 {
+    //! TODO
+    //!
+    //! See crate [`::blake3`].
+    pub use ::blake3::*;
+}
 
 #[cfg(feature = "byteorder")]
-pub use byteorder;
+pub mod byteorder {
+    //! TODO
+    //!
+    //! See crate [`::byteorder`].
+    pub use ::byteorder::*;
+}
 
 #[cfg(feature = "bytes")]
-pub use bytes;
+pub mod bytes {
+    //! Abstractions for working with byte buffers: [`Bytes`], [`Buf`], and [`BufMut`].
+    //!
+    //! See crate [`bytes`].
+    pub use ::bytes::*;
+}
