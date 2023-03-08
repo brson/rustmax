9e2fe8fd4837b7134968b13d19508795a5f17a07
Brian Anderson <andersrb@gmail.com>
2023-03-07 22:13:38 -0700

    .

--- crates/rustx/src/lib.rs
+++ crates/rustx/src/lib.rs
@@ -1,4 +1,88 @@
-//! TODO
+//! A collection of useful Rust crates.
+//!
+//! This crate documents and reexports selected high-quality Rust crates
+//! suitable for typical Rust programs.
+//! It can be thought of as a "batteries-included" standard library.
+//!
+//! # Using `rustx` for crate discovery.
+//!
+//! If you just need to know decent crates for common programming tasks,
+//! read this documentation to find them.
+//!
+//! # Using `rustx` as a library.
+//!
+//! todo
+//!
+//! In your manifest `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! rustx = "0.1.0"
+//! ```
+//!
+//! Or if using a workspace, in your workspace `Cargo.toml`
+//!
+//! ```toml
+//! [workspace.dependencies]
+//! rustx = "0.1.0"
+//! ```
+//!
+//! And in your manifest `Cargo.toml`
+//!
+//! ```toml
+//! [dependencies]
+//! rustx.workspace = true
+//! ```
+//!
+//! # Feature profiles
+//!
+//! The main way of configuring the `rustx` crates is by enabling
+//! the appropriate _profile_ cargo features.
+//!
+//! The default profile feature is `rx-profile-std-plus`.
+//! This feature augments the Rust `std` library with crates
+//! that are widely used with a variety of Rust programs,
+//! as well as minor helpers missing from the standard library.
+//!
+//! If the default features are disabled:
+//!
+//! ```toml
+//! [dependencies]
+//! rustx.version = "0.1.0"
+//! rustx.default-features = false
+//! ```
+//!
+//! Then `rustx` reexports no crates.
+//! Profiles can then be added by adding cargo features:
+//!
+//! ```toml
+//! [dependencies]
+//! rustx.version = "0.1.0"
+//! rustx.default-features = false
+//! rustx.features = ["rx-profile-no-std"]
+//! ```
+//!
+//! ## Profile: `rx-profile-no-std`
+//!
+//! This profile includes crates that do not require Rust `std`,
+//! and provide features used by many Rust programs.
+//!
+//! Crates in this profile:
+//!
+//! - [`anyhow`]
+//!
+//! ## Profile: `rx-profile-std-plus`
+//!
+//! ## Profile: `rx-profile-net`
+//!
+//! ## Profile: `rx-profile-cli`
+//!
+//! ## Profile: `rx-profile-build-script`
+//!
+//! ## Profile: `rx-profile-proc-macro`
+//!
+//! ## Profile: `rx-profile-full`
+//!
 //!
 //! # Features
 //!
