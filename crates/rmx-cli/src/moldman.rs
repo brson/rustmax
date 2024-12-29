792a60a2cd0f2bfb72673a28a158e810945c5b48
Brian Anderson <andersrb@gmail.com>
2024-12-29 16:35:45 -0700

    mold

--- crates/rmx-cli/src/moldman.rs
+++ crates/rmx-cli/src/moldman.rs
@@ -1,6 +1,17 @@
 use rmx::prelude::*;
+use rmx::tempfile::TempDir;
+use std::env;
+use std::path::PathBuf;
+
 
 pub fn install() -> AnyResult<()> {
+    let tempdir = TempDir::with_prefix_in(
+        "mold-install",
+        env::current_dir()?,
+    )?;
+
+    //let path = download_release_tarball(tempdir.path())?;
+
     todo!()
 }
 
@@ -16,3 +27,26 @@ pub fn status() -> AnyResult<()> {
     todo!()
 }
 
+enum MoldTarget {
+    LinuxX86_64,
+}
+
+fn get_target() -> AnyResult<MoldTarget> {
+    if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") {
+        Ok(MoldTarget::LinuxX86_64)
+    } else {
+        Err(anyhow!("unsupperted target"))
+    }
+}
+
+fn get_current_release() -> AnyResult<String> {
+    todo!()
+}
+  
+fn get_url() -> AnyResult<String> {
+    todo!()
+}
+
+fn get_cargo_bin_dir() -> AnyResult<PathBuf> {
+    todo!()
+}
