f042ad096c50d8204203141f9bc7683a0320e97b
Brian Anderson <andersrb@gmail.com>
2025-01-18 01:30:49 -0700

    .

--- www/script.js
+++ www/script.js
@@ -4,16 +4,12 @@ window.addEventListener("load", () => {
         const name = button.dataset.name;
         const exampleRow = document.querySelector(`#example-row-${name}`);
 
-        console.log(button);
-
         button.addEventListener("click", () => {
             exampleRow.classList.toggle("example-row-visible");
             if (exampleRow.classList.contains("example-row-visible")) {
                 button.innerText = "-";
-                console.log(button);
             } else {
                 button.innerText = "+";
-                console.log(button);
             }
         });
     }
