322db6b4e840f5a40027021a22d55650863f6585
Brian Anderson <andersrb@gmail.com>
2025-02-09 18:55:37 -0700

    script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 set +euxo pipefail
 
+TOOLCHAIN=1.84.1
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain $TOOLCHAIN
 cargo install mdbook@0.4.43
 cargo install just@1.36.0
 
