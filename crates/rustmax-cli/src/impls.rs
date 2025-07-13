2538355d369ab00918abc2877d47eb647bfeed17
Brian Anderson <andersrb@gmail.com>
2025-07-13 12:56:54 -0600

    deadcode

--- crates/rustmax-cli/src/impls.rs
+++ crates/rustmax-cli/src/impls.rs
@@ -60,56 +60,23 @@ impl Tool {
             CargoAudit => ToolAttrs {
                 display_name: "cargo-audit",
             },
-            CargoBenchcmp => ToolAttrs {
-                display_name: "cargo-benchcmp",
-            },
             CargoCleanAll => ToolAttrs {
                 display_name: "cargo-clean-all",
             },
             CargoDeny => ToolAttrs {
                 display_name: "cargo-deny",
             },
-            CargoDeps => ToolAttrs {
-                display_name: "cargo-deps",
-            },
             CargoEdit => ToolAttrs {
                 display_name: "cargo-edit",
-            },
-            CargoExpand => ToolAttrs {
-                display_name: "cargo-expand",
-            },
-            CargoFuzz => ToolAttrs {
-                display_name: "cargo-fuzz",
-            },
-            CargoGeiger => ToolAttrs {
-                display_name: "cargo-geiger",
             },
             CargoGenerate => ToolAttrs {
                 display_name: "cargo-generate",
             },
-            CargoHack => ToolAttrs {
-                display_name: "cargo-hack",
-            },
-            CargoLlvmLines => ToolAttrs {
-                display_name: "cargo-llvm-lines",
-            },
             CargoOutdated => ToolAttrs {
                 display_name: "cargo-outdated",
             },
-            CargoUdeps => ToolAttrs {
-                display_name: "cargo-udeps",
-            },
             CargoTree => ToolAttrs {
                 display_name: "cargo-tree",
-            },
-            CargoWatch => ToolAttrs {
-                display_name: "cargo-watch",
-            },
-            CargoWorkspace => ToolAttrs {
-                display_name: "cargo-workspace",
-            },
-            CargoSemver => ToolAttrs {
-                display_name: "cargo-semver",
             },
 
             /* non-rust */
