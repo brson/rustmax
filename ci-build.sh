842c7d78c76b9c90de47c29ea4bb06168d0d1743
Brian Anderson <andersrb@gmail.com>
2025-09-22 14:16:30 -0600

    ci

--- ci-build.sh
+++ ci-build.sh
@@ -3,12 +3,11 @@
 set +euxo pipefail
 
 # Unfortunate this is needed.
-sudo apt install libasound2-dev
+apt install libasound2-dev
 
 TOOLCHAIN=1.88.0
-rustup toolchain default $TOOLCHAIN
+rustup default $TOOLCHAIN
 
-cargo install just@1.36.0
-
-just install-tools
+cargo install mdbook
+cargo install mdbook-yapp
 RUSTMAX_CI=1 just build
